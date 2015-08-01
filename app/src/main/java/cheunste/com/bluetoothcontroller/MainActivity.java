6a29a617d9375d825530c79364410a0e49527e0c
Stephen <cheunste@gmail.com>
2015-08-01 12:27:18 -0700

    Made mBluetoothAdapter a global variable in MainActivity.java

--- app/src/main/java/cheunste/com/bluetoothcontroller/MainActivity.java
+++ app/src/main/java/cheunste/com/bluetoothcontroller/MainActivity.java
@@ -1,6 +1,7 @@
 package cheunste.com.bluetoothcontroller;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -9,19 +10,20 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    BluetoothAdapter mBluetoothAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth
         }
-//        if (mBluetoothAdapter.isEnabled()) {
-//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//            startActivityForResult(enableBtIntent, 1);
-//        }
+        if (mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
     }
 
 
